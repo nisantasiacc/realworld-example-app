06d883bd624ea8c3943037a345c8322d52689169
nisantasiacc <nisantasimail32@gmail.com>
2025-11-20 19:37:44 +0000

    deneme

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(err) {
     if (err) {
         console.log(`Error: ${err.message}`);
+        // deneme
     }
     
     console.log("Server is listening on Port", PORT);
